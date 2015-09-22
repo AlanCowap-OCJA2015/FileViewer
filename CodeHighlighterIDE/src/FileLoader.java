fd254b05cb410a6f7347322fc82cbc7526f1412a
ArtjomsPorss <artjoms.porss@gmail.com>
2015-09-22 22:34:42 +0100

    added locating keywords folder
    refactored FileLoader and Window classes
    modified highlighting algorithm

--- CodeHighlighterIDE/src/FileLoader.java
+++ CodeHighlighterIDE/src/FileLoader.java
@@ -1,21 +1,113 @@
-//Loads text from file, returns text using toString()
-
+//Loads text and keywords from file
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class FileLoader {
-
+	private String codePath = "";	//stores path to code file
+	private String keywordPath = "";
 	
 	//add constructor which loads text
 	public FileLoader(){}
+	
+	public String getCodeExtention(){
+		String extention = "";
+		for (int i = codePath.length() - 1; i >= 0 ; i--) {
+			if(codePath.charAt(i) == '.'){
+				extention = codePath.substring(i+1);
+			}
+		}
+		return extention;
+	}
+	
+	
+	//set new keyword path
+	public void setKeywordsPath(){
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		File keywordFile;
+		JButton btnLoadFile = null;
+		Scanner scan = null;
+		int returnVal = chooser.showOpenDialog(btnLoadFile);
+		String text = "";
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION){
+			keywordFile = chooser.getSelectedFile();
+			keywordPath = keywordFile.getAbsolutePath() + "\\";
+			
+			//TODO to remove
+			System.out.println(keywordPath);
+		}
+	}
+	
+	
+	public ArrayList<String> loadKeywords(){
+		String extention = getCodeExtention();
+		//System.out.println(extention);
+		if(keywordPath.equals("")){ 
+			keywordPath = this.getClass().getClassLoader().getResource("keywords/").getPath(); 
+		}
+		//System.out.println(keywordPath);
+		
+		ArrayList<String> keywords = new ArrayList<String>(80);
+		String keywordText = loadTextFromFile(keywordPath + extention + ".txt");
+		//System.out.println(keywordText);
+		int start = 0;
+		for (int i = 0; i < keywordText.length(); i++) {
+			if(keywordText.charAt(i) == 10){
+				keywords.add(keywordText.substring(start, i));
+				
+				//System.out.println(keywordText.substring(start, i));	//TEST
+				start = i+1;
+			}
+		}
+		
+		return keywords;
+	}
+	
+	
+	public String loadFile(){
+		File codeFile;
+		JFileChooser chooser = new JFileChooser();
+		JButton btnLoadFile = null;
+		Scanner scan = null;
+		int returnVal = chooser.showOpenDialog(btnLoadFile);
+		String text = "";
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION){
+			codeFile = chooser.getSelectedFile();
+			codePath = codeFile.getAbsolutePath();
 
-	//load text from file
+			try {
+				scan = new Scanner(new FileReader(codeFile));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(!scan.equals(null)){
+				while(scan.hasNextLine()){
+					text += scan.nextLine().toString() + "\n";
+				}
+			}			
+		}
+		
+		return text;
+	}
+
+	
+
 	public String loadTextFromFile(String filePath){
 		String text = "";
 		
@@ -29,7 +121,7 @@ public class FileLoader {
 				} else {
 					text += tempLine + "\n";
 					while((tempLine = br.readLine()) != null){
-						text += tempLine;
+						text += tempLine + "\n";
 					}
 				}
 			}
--- CodeHighlighterIDE/src/Window.java
+++ CodeHighlighterIDE/src/Window.java
@@ -1,5 +1,5 @@
 /*
-* GUI for the code highlighter
+* GUI for the code highlighter app
 */
 
 
@@ -19,13 +19,15 @@ import javax.swing.JScrollBar;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Highlighter;
+import javax.swing.border.BevelBorder;
 
 
 public class Window {
 
-	private JFrame frame;
+	private JFrame frmCodeHighlighter;
 	private JTextArea textArea;
 	public static JFrame window;
+
 
 
 	/**
@@ -36,7 +38,7 @@ public class Window {
 			public void run() {
 				try {
 					Window window = new Window();
-					window.frame.setVisible(true);
+					window.frmCodeHighlighter.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -48,7 +50,7 @@ public class Window {
 	 * Create the application.
 	 */
 	public Window() {
-		window = frame;
+		window = frmCodeHighlighter;
 		initialize();
 	}
 
@@ -56,27 +58,41 @@ public class Window {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 507, 506);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmCodeHighlighter = new JFrame();
+		frmCodeHighlighter.setTitle("Code Highlighter");
+		frmCodeHighlighter.setBounds(100, 100, 507, 506);
+		frmCodeHighlighter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCodeHighlighter.getContentPane().setLayout(null);
 		
 		textArea = new JTextArea();
+		textArea.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		textArea.setEditable(false);
 		textArea.setBounds(10, 11, 471, 400);
-		frame.getContentPane().add(textArea);
+		frmCodeHighlighter.getContentPane().add(textArea);
 		
 		JButton btnLoadCode = new JButton("Load Code");
 		btnLoadCode.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				//Logic.getCodeExtention();
-				textArea.setText(Logic.loadTextFile());
+				String text = Logic.loadTextFile();
+				textArea.setText(text);
 				highlightCode();
 				//Logic.loadKeywords();
 			}
 		});
-		btnLoadCode.setBounds(392, 422, 89, 23);
-		frame.getContentPane().add(btnLoadCode);
+		btnLoadCode.setBounds(251, 422, 230, 23);
+		frmCodeHighlighter.getContentPane().add(btnLoadCode);
+		
+		JButton btnKeywordFolder = new JButton("Keyword Folder");
+		btnKeywordFolder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Logic.locateKeywordsFolder();
+			}
+		});
+		btnKeywordFolder.setBounds(10, 422, 230, 23);
+		frmCodeHighlighter.getContentPane().add(btnKeywordFolder);
+		
 	}
 	
 	
@@ -96,12 +112,29 @@ public class Window {
 					continue outer;
 				}
 				
-				try {
-					hl.addHighlight(pos, pos + keyword.length(), DefaultHighlighter.DefaultPainter);
-				} catch (BadLocationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				if(pos == 0 && validCharacter(text.charAt(pos + keyword.length()))){
+					try {
+						hl.addHighlight(pos, pos + keyword.length(), DefaultHighlighter.DefaultPainter);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}else if(validCharacter(text.charAt(pos - 1)) && pos + keyword.length() == text.length()-1){
+					try {
+						hl.addHighlight(pos, pos + keyword.length(), DefaultHighlighter.DefaultPainter);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}else if(validCharacter(text.charAt(pos - 1)) && validCharacter(text.charAt(pos + keyword.length()))){
+					try {
+						hl.addHighlight(pos, pos + keyword.length(), DefaultHighlighter.DefaultPainter);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
+				
 				pos++;
 			}while(pos != -1);
 		}
@@ -110,5 +143,18 @@ public class Window {
 		//if keyword is in the end of file - check only start
 		//if it's in the middle of a file - check start and end for letters {}[]() or space - it's a keyword
 		
+	}
+	
+	
+	private boolean validCharacter(char c){
+		switch(c){
+		case ' ' : return true;
+		case '[' : return true;
+		case '{' : return true;
+		case '(' : return true;
+		case 9 	: return true;
+		case 10 : return true;
+		default : return false;
+		}
 	}
 }
