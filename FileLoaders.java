989c6d79d4b5a4f4446a36395a0829827986f1c4
Kevin Phair <scorpio@xdi.nu>
2015-09-23 14:37:01 +0100

    unified parameter names between methods

--- FileLoaders.java
+++ FileLoaders.java
@@ -39,23 +39,22 @@ public class FileLoaders {
 	 * @param file name of source file
 	 * @return String containing all the lines of the file delimited by newlines.
 	 */
-	public void read (String inputFile, JTextArea jta) {
+	public void read (String filename, JTextArea jta) {
 		
 		// If filename is empty then bring up a file selector window to pick one
-		if (inputFile == null || inputFile.length() == 0) {
-			JFileChooser jfc = new JFileChooser(System.getProperty("user.dir"));
-			jfc.showOpenDialog(null);
-			inputFile = jfc.getSelectedFile().getPath();
+		if (filename == null || filename.length() == 0) {
+			filename = getFilename();
 		}
 
 		// Make sure input file name is valid before trying to open it
-	    if (inputFile != null && inputFile.length() > 0) {
+	    if (filename != null && filename.length() > 0) {
 	        // open file for input
 	        try {
-	    	    BufferedReader inFile = new BufferedReader(new FileReader(inputFile));
+	    	    BufferedReader inFile = new BufferedReader(new FileReader(filename));
 	    	    Thread rw = new Thread(new readWorker(inFile, jta));
 	    	    System.out.println("Spawning new readWorker...");
 	    	    rw.start();
+	    	    System.out.println("readWorker started.");
 	        } catch (IOException e) {
 	        	e.printStackTrace();
 	        }
@@ -63,14 +62,16 @@ public class FileLoaders {
 	}
 
 	/**
-	 * a readWorker thread that will read the file data in and update the
+	 * A readWorker thread that will read the file data in and update the
 	 * JTextArea when done
 	 */
 	class readWorker implements Runnable {
 		BufferedReader reader;
 		JTextArea jta;
 		
-		// Initialise the object with the opened reader for reading in the file
+		/* Initialise the object with the opened reader for reading in the file
+		 * and the JTextArea which needs to be updated when done
+		 */
 		readWorker (BufferedReader r, JTextArea j) {
 			this.reader = r;
 			this.jta = j;
@@ -80,6 +81,7 @@ public class FileLoaders {
 		public void run() {
 			String fileData = "";
     	    String inLine = null;
+    	    System.out.println("readWorker is reading file.");
     	    try {
     	    	while (true) {
     	    		inLine = reader.readLine();
@@ -87,12 +89,12 @@ public class FileLoaders {
     	    		fileData += inLine + "\n";
     	    	}
     	    	reader.close();
+        	    System.out.println("readWorker is done.");
 	        } catch (IOException e) {
 	        	e.printStackTrace();
 	        }
     	    jta.setText(fileData);
 		}
-		
 	}
 
 	/**
@@ -100,21 +102,19 @@ public class FileLoaders {
 	 * @param file name of source file
 	 * @return String containing all the lines of the file delimited by newlines.
 	 */
-	public String blockingRead (String inputFile) {
+	public String blockingRead (String filename) {
 		String fileData = "";
 
 		// If filename is empty then bring up a file selector window to pick one
-		if (inputFile == null || inputFile.length() == 0) {
-			JFileChooser jfc = new JFileChooser(System.getProperty("user.dir"));
-			jfc.showOpenDialog(null);
-			inputFile = jfc.getSelectedFile().getPath();
+		if (filename == null || filename.length() == 0) {
+			filename = getFilename();
 		}
 
 		// Make sure input file name is valid before trying to open it
-	    if (inputFile != null && inputFile.length() > 0) {
+	    if (filename != null && filename.length() > 0) {
 	        // open file for input
 	        try {
-	    	    BufferedReader inFile = new BufferedReader(new FileReader(inputFile));
+	    	    BufferedReader inFile = new BufferedReader(new FileReader(filename));
 	    	    String inLine = null;
 	            while (true) {
 	            	inLine = inFile.readLine();
@@ -142,9 +142,7 @@ public class FileLoaders {
 		
 		// If filename is empty then bring up a file selector window to pick one
 		if (filename == null || filename.length() == 0) {
-			JFileChooser jfc = new JFileChooser(System.getProperty("user.dir"));
-			jfc.showOpenDialog(null);
-			filename = jfc.getSelectedFile().getPath();
+			filename = getFilename();
 		}
 
 		// Make sure input file name is valid before trying to open it
@@ -161,5 +159,13 @@ public class FileLoaders {
 	    }
 	}
 	
+	/**
+	 * Bring up the file selector window to get a filename
+	 */
+	static public String getFilename () {
+		JFileChooser jfc = new JFileChooser(System.getProperty("user.dir"));
+		jfc.showOpenDialog(null);
+		return jfc.getSelectedFile().getPath();
+	}
 }
 
