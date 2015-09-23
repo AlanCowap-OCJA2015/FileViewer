4671a04779897a76e6ef16cbeb6c7eb7a0ca3bf5
JennQuinn42 <jennquinn42@gmail.com>
2015-09-23 14:52:29 +0100

    Update GUIApp.java
    Can now invert colours of GUI,
    Defaults text field to uneditable, but have the option of putting in password to make it editiable

--- GUIApp.java
+++ GUIApp.java
@@ -1,29 +1,48 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
-import java.awt.FlowLayout;
+import javax.swing.text.Highlighter;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
 
+@SuppressWarnings("serial")
 public class GUIApp extends JFrame {
 
 	private JPanel contentPane;
+	private JScrollPane scrollPane;
 	private JTextArea txtEditor;
+	private JMenuBar mnuEditor;
+	private JMenu mnFile;
+	private JMenu mnLanguage;
+	private JMenu mnTheme;
+	private JMenuItem mntmOpen;
+	private JMenuItem mntmSave;
+	private JMenuItem mntmJava;
+	private JMenuItem mntmC;
+	private JMenuItem mntmInvert;
+	private JMenuItem mntmCustomise;
+	private Color defaultColour;
 	private FileLoaders load = new FileLoaders();
 	private GDKHighlighter highlight = new GDKHighlighter();
 	String strLanguage;
+	private JMenuItem mntmEditable;
+	private char[] adminPassword = {'p', 'a','s', 's', 'w', 'o', 'r', 'd'};
 	/**
 	 * Launch the application.
 	 */
@@ -44,20 +63,21 @@ public class GUIApp extends JFrame {
 	 * Create the frame.
 	 */
 	public GUIApp() {
+		getContentPane().setBackground(Color.LIGHT_GRAY);
 		setTitle("GDK IDE");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 590, 528);
-		
-		JMenuBar mnuEditor = new JMenuBar();
+		defaultColour = getBackground();
+		mnuEditor = new JMenuBar();
 		setJMenuBar(mnuEditor);
 		
-		JMenu mnFile = new JMenu("File");
+		mnFile = new JMenu("File");
 		mnuEditor.add(mnFile);
 		
-		JMenuItem mntmOpen = new JMenuItem("Open");
+		mntmOpen = new JMenuItem("Open");
 		mntmOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				txtEditor.setText(load.read(""));
+				load.read("", txtEditor);
 				if(strLanguage != null){
 					highlight(strLanguage);
 				}
@@ -65,28 +85,105 @@ public class GUIApp extends JFrame {
 		});
 		mnFile.add(mntmOpen);
 		
-		JMenuItem mntmSave = new JMenuItem("Save");
+		mntmSave = new JMenuItem("Save");
+		mntmSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				load.save(txtEditor.getText(), "");
+			}
+		});
 		mnFile.add(mntmSave);
 		
-		JMenu mnLanguage = new JMenu("Language");
+		mntmEditable = new JMenuItem("Editable");
+		mntmEditable.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(mntmEditable.getText().equals("Editable")){
+					JPanel panel = new JPanel();
+					JLabel label = new JLabel("Enter password:");
+					JPasswordField pass = new JPasswordField(10);
+					panel.add(label);
+					panel.add(pass);
+					String[] options = new String[]{"OK", "Cancel"};
+					int option = JOptionPane.showOptionDialog(null, panel, "The title",
+					                         JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
+					                         null, options, options[1]);
+					if(option == 0) // pressing OK button
+					{
+					    if(isPassword(pass.getPassword())){
+					    	txtEditor.setEditable(true);
+					    	mntmEditable.setText("Uneditable");
+					    }
+					}		
+				}else{
+					mntmEditable.setText("Editable");
+					txtEditor.setEditable(false);
+				}
+					
+			}
+		});
+		mnFile.add(mntmEditable);
+		
+		mnLanguage = new JMenu("Language");
 		mnuEditor.add(mnLanguage);
 		
-		JMenuItem mntmJava = new JMenuItem("Java");
+		mntmJava = new JMenuItem("Java");
 		mntmJava.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				strLanguage = "Java";
 				if(txtEditor.getText().length() != 0){
-					highlight("Java");
+					highlight(strLanguage);
 				}
 			}
 		});
 		mnLanguage.add(mntmJava);
+		
+		mntmC = new JMenuItem("C#");
+		mntmC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				strLanguage = "CSharp";
+				if(txtEditor.getText().length() != 0){
+					highlight(strLanguage);
+				}
+			}
+		});
+		mnLanguage.add(mntmC);
+		
+		mnTheme = new JMenu("Theme");
+		mnuEditor.add(mnTheme);
+		
+		mntmInvert = new JMenuItem("Invert");
+		mntmInvert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(getBackground() != Color.DARK_GRAY){
+					highlight.setHighLightColor(Color.CYAN);
+					highlight.setHighlightDarkness();
+					setBackgroundColour(Color.DARK_GRAY);
+					txtEditor.setBackground(Color.DARK_GRAY);
+					txtEditor.setForeground(Color.LIGHT_GRAY);
+					setMenuColour(Color.DARK_GRAY);
+					setMenuFontColour(Color.LIGHT_GRAY);
+					setFontColour(Color.LIGHT_GRAY);
+				}else{
+					highlight.setHighLightColor(Color.ORANGE);
+					setBackgroundColour(defaultColour);
+					txtEditor.setBackground(Color.WHITE);
+					txtEditor.setForeground(Color.BLACK);
+					setMenuColour(defaultColour);
+					setMenuFontColour(Color.BLACK);
+					setFontColour(Color.BLACK);
+				}
+			}
+		});
+		mnTheme.add(mntmInvert);
+		
+		mntmCustomise = new JMenuItem("Customise");
+		mnTheme.add(mntmCustomise);
 		contentPane = new JPanel();
 		contentPane.setAutoscrolls(true);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
+		scrollPane.setBackground(new Color(240, 240, 240));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -98,8 +195,26 @@ public class GUIApp extends JFrame {
 		);
 		
 		 txtEditor = new JTextArea();
+		 txtEditor.setEditable(false);
+		 txtEditor.addKeyListener(new KeyAdapter() {
+		 	@Override
+		 	public void keyTyped(KeyEvent arg0) {
+		 		highlight(strLanguage);
+		 	}
+		 });
+		 txtEditor.setFont(new Font("Times New Roman", Font.PLAIN, 14));
 		scrollPane.setViewportView(txtEditor);
 		contentPane.setLayout(gl_contentPane);
+	}
+	
+	private boolean isPassword(char[] userPassword){
+		for(int i = 0; i < userPassword.length; ++i){
+			if(userPassword[i] != adminPassword[i]){
+				JOptionPane.showMessageDialog(null, "Incorrect Password");
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private void highlight(String language){
@@ -111,4 +226,45 @@ public class GUIApp extends JFrame {
 			highlight.highlight(keywords, txtEditor);
 		}
 	}
+	
+	private void setBackgroundColour(Color newColour){
+		setBackground(newColour);
+		contentPane.setBackground(newColour);
+		scrollPane.setBackground(newColour);
+		mnuEditor.setBackground(newColour);
+		//txtEditor.setBackground(newColour);
+	}
+	
+	private void setMenuColour(Color newColour){
+		mnFile.setBackground(newColour);
+		mnLanguage.setBackground(newColour);
+		mnTheme.setBackground(newColour);
+		mntmC.setBackground(newColour);
+		mntmCustomise.setBackground(newColour);
+		mntmInvert.setBackground(newColour);
+		mntmJava.setBackground(newColour);
+		mntmOpen.setBackground(newColour);
+		mntmSave.setBackground(newColour);
+	}
+	
+	private void setMenuFontColour(Color newColour){
+		mnFile.setForeground(newColour);
+		mnLanguage.setForeground(newColour);
+		mnTheme.setForeground(newColour);
+		mntmC.setForeground(newColour);
+		mntmCustomise.setForeground(newColour);
+		mntmInvert.setForeground(newColour);
+		mntmJava.setForeground(newColour);
+		mntmOpen.setForeground(newColour);
+		mntmSave.setForeground(newColour);
+		
+	}
+	
+	private void setFontColour(Color newColour){
+		//txtEditor.setForeground(newColour);
+		mnFile.setForeground(newColour);
+		mnLanguage.setForeground(newColour);
+		mnTheme.setForeground(newColour);
+	}
+	
 }
