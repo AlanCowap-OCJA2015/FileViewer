9b42cb2dd739663a5610d10485ab54ebe2dfe650
Artjoms Porss <artjoms.porss@gmail.com>
2015-09-23 15:32:46 +0100

    fileLoader loads in separate thread

--- CodeHighlighterIDE/src/Logic.java
+++ CodeHighlighterIDE/src/Logic.java
@@ -4,17 +4,19 @@
 
 import java.util.ArrayList;
 
-public class Logic {
+public class Logic{
 	public static FileLoader fl = new FileLoader();
 	
 	private static ArrayList<String> keywords = new ArrayList<String>(60);	//keywords from keywordPath + extention
 	
 	
-	public static String loadTextFile(){
-		String textFromFile = fl.loadFile();
+	public static synchronized String loadTextFile(){
+		
+		String loadedText = fl.loadFile();
 		keywords = fl.loadKeywords();
 		//System.out.println(keywords);
-		return textFromFile;
+
+		return loadedText;
 	}
 	
 	
